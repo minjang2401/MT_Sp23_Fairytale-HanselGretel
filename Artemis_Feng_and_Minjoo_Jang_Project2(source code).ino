1c714305650c2ea0514bd0fc82e0ed9f2d546e1e
Minjoo Jang <129447045+minjang2401@users.noreply.github.com>
2023-04-04 12:11:56 -0500

    added LED Pin
    led pin onto the Minjoo Branch

--- Artemis_Feng_and_Minjoo_Jang_Project2(source code).ino
+++ Artemis_Feng_and_Minjoo_Jang_Project2(source code).ino
@@ -2,9 +2,11 @@
 
 Servo myservo;
 const int buttonPin = 2;
+const int ledPin = 13;
 void setup() {
   myservo.attach(9);
   pinMode(butonPin, INPUT);
+  pinMode(ledPin, INPUT);
 
 }
 
@@ -14,6 +16,12 @@ void loop() {
   }
   else{
     myservo.write(0);
+  }
+  if(digitalRead(ledPin)==HIGH){
+    digitalWrite(ledPin, HIGH);
+  }
+  else{
+    digitalWrite(ledPin, LOW);
   }
 
 }
